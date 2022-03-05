53073041571ad382fc43aeaaf3128221ed161294
N-Shar-ma <bablinaneh@gmail.com>
2022-03-05 22:24:04 +0530

    Add endpoint for getting profile by roll number

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,11 +2,11 @@ const data = require('../profiles.json')
 const express = require("express")
 const router = express.Router()
 
-// router.get('/:language', (req, res) => { // returns hello world program in the requested language
-//     const result = data.find(code => code.language_name.toLowerCase() === req.params.language.toLowerCase())
-//     if(result) res.send(result)
-//     else res.sendStatus(404)
-// })
+router.get('/:roll_number', (req, res) => { // returns profile with the requested roll number
+    const result = data.find(profile => profile.roll_number.toLowerCase() === req.params.roll_number.toLowerCase())
+    if(result) res.send(result)
+    else res.sendStatus(404)
+})
 
 router.get('/', (req, res) => { // returns profiles of all the participants
     res.send(data)
